4fec059f571607cb96320940de5a13d398635f40
johnalexjitu-pixel <johnalexjitu@gmail.com>
2025-10-28 11:01:25 +0600

    Disable ESLint rules for withdrawal page)

--- src/app/account/withdrawal/page.tsx
+++ src/app/account/withdrawal/page.tsx
@@ -1,5 +1,6 @@
- "use client";
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
 import { useEffect, useState } from "react";
 import { apiClient } from "@/lib/api-client";
 import { useAuth } from "@/contexts/AuthContext";
